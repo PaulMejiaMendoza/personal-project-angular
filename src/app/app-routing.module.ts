a1a1bf3f561de16331503eab486ce1239d6ceb6e
Paul Mejia Mendoza <mejillas_33@hotmail.com>
2020-03-20 04:24:02 +0100

    he creado de nuevo el navbar y el grid de la pagina principal del home con bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import { NgModule }              from '@angular/core';
-import { RouterModule, Routes }  from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { TherapistInfoComponent } from './components/therapist-info/therapist-info.component';
+import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './components/error404/error404.component';
 
 
 
 const appRoutes: Routes = [
-    {path: 'login', component: LoginComponent}, 
     {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
     {path: 'therapist/:therapistId', component: TherapistInfoComponent},
     {path: '404', component: Error404Component},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BodySearchComponent } from './components/body-search/body-search.component';
-import { BodyTreatmentsComponent } from './components/body-treatments/body-treatments.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { MySteticServicesComponent } from './components/my-stetic-services/my-stetic-services.component';
 import { TherapistInfoComponent } from './components/therapist-info/therapist-info.component';
 import { Error404Component } from './components/error404/error404.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    BodySearchComponent,
-    BodyTreatmentsComponent,
     LoginComponent,
     HomeComponent,
     MySteticServicesComponent,
     TherapistInfoComponent,
-    Error404Component
+    Error404Component,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     AppRoutingModule,
   ],
   providers: [],
